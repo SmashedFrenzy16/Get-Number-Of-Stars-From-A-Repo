1bbb6a2e50e4ea497bea364a52e188b556fc5cd4
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-07-30 13:35:51 +0500

    Update repo-stars.py

--- repo-stars.py
+++ repo-stars.py
@@ -1,1 +1,9 @@
 from github import Github
+
+g_cred = Github("<Paste Your Token Here>")
+
+f_repo = input("Enter repository name to see its stars (eg. User/Repo): ")
+
+repo = g_cred.get_repo(f_repo)
+
+print(repo.stargazers_count)
